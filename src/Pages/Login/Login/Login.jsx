904b0f3032e385af4387a70ea19117a18c2326b1
Md Saidur Rahman <saidurrahmanbanguri1997@gmail.com.com>
2023-05-19 01:21:01 +0600

    register section added

---
+++ src/Pages/Login/Login/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+              
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                    <h2 className='text-2xl text-center'>Please Login</h2>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                        <p className='mt-3'>New To Baby Mart?<Link to='/registration'> Please Sign Up</Link></p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
